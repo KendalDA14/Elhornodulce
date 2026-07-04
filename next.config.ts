74580d6864dca87323344fe4828fe0405a216da6
KendalDA14 <kzuaraya@gmail.com>
2026-07-04 12:59:47 -0600

    limit next build workers for hostinger

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   output: "standalone",
+  poweredByHeader: false,
+  experimental: {
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
